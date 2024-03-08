db54b67fa16ebb4116b842596bec10ea391bf02a
Arijana Marić <maricarijana69@gmail.com>
2024-03-08 18:12:01 +0100

    Add model Language

---
+++ Model/Language.cs
@@ -0,0 +1,45 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Language : ISerializable
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }    
+
+
+        public Language() { }
+
+        public Language(int id, string name)
+        {
+            Id=id;
+            Name=name;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+
+
+                Id.ToString(),
+                Name
+
+
+            };
+
+            return csvValues;
+        }
+    }
+}
