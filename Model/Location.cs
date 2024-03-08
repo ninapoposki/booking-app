f8090ff49835072f64f00d430a3bd088f7ac20a2
Arijana Marić <maricarijana69@gmail.com>
2024-03-08 17:51:17 +0100

    Add Image and Location model

---
+++ Model/Location.cs
@@ -0,0 +1,55 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Location : ISerializable
+    {
+
+
+
+
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Location() { }
+        
+        public Location(int id, string city, string country)
+        {
+
+            Id = id;
+            City = city;
+            Country = country;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            City = values[1];
+            Country = values[2];
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                City,
+                Country
+
+
+            };
+
+
+            return csvValues;
+            
+        }
+
+
+        
+    }
+}
