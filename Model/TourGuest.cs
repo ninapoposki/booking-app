0888ddc3f83dd3622912c7836e879fa68ea75b3c
Arijana Marić <maricarijana69@gmail.com>
2024-03-09 13:33:27 +0100

    Add models TourGuest and TourReservation

---
+++ Model/TourGuest.cs
@@ -0,0 +1,47 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class TourGuest : ISerializable
+    {
+        int Id { get; set; }
+        public string FullName { get; set; }    
+        public int Age { get; set; }
+
+        public TourGuest (int  id, string fullName, int age)
+        {
+            Id = id;
+            FullName = fullName;
+            Age = age;
+        }
+        public TourGuest() { }
+
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            FullName = values[1];
+            Age = Convert.ToInt32(values[2]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+
+                Id.ToString(),
+                FullName,
+                Age.ToString()
+
+
+            };
+
+            return csvValues;
+        }
+    }
+}
