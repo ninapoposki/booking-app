74b15c9f0c280d3efb3714f581933b9b43e630ee
Arijana Marić <maricarijana69@gmail.com>
2024-03-10 17:54:11 +0100

    Add WPF for Tourist

---
+++ View/Tourist/TouristMainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using BookingApp.Observer;
+using System;
+using BookingApp.Repository;
+using BookingApp.Model;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.Tourist
+{
+    /// <summary>
+    /// Interaction logic for TouristMainWindow.xaml
+    /// </summary>
+    public partial class TouristMainWindow : Window,IObserver
+    {
+
+        private TourReporsitory tourRepository; 
+        public List<Tour> AllTours { get; set; }
+        public TouristMainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            AllTours = tourRepository.GetAll();
+            Update();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonCancel(object sender, RoutedEventArgs e)
+        {
+
+
+            this.Close();
+        }
+
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+
+            
+
+
+        }
+    }
+}
