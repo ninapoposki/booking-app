6d430e9cb2c82818f59b1bee236c9908248230fe
Anja Srdić <srdicanjaa31@gmail.com>
2024-04-15 03:08:51 +0200

    Pitcures

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -38,7 +38,12 @@ namespace BookingApp.Services
         public ImageDTO GetByPath(string relativePath) {
             return new ImageDTO(imageRepository.FindByPath(relativePath));
         }
-
+        public string GetFirstPath(int entityId,string type)
+        {
+            Image? image=imageRepository.GetAll().Find(i => i.EntityId == entityId && i.EntityType.ToString().Equals(type));
+            if (image != null) return image.Path;
+            return null;
+        }
         public List<ImageDTO> GetAll()
         {
             var images = new List<ImageDTO>();
---
+++ WPF/View/Guide/TourReviews.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BookingApp.DTO;
+using BookingApp.WPF.ViewModel.Guide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.WPF.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for TourReviews.xaml
+    /// </summary>
+    public partial class TourReviews : Window
+    {
+        public TourReviewsVM TourReviewsVM { get; set; }
+        public TourReviews(TourDTO tour)
+        {
+            InitializeComponent();
+            TourReviewsVM=new TourReviewsVM(tour);
+            DataContext = TourReviewsVM;
+        }
+    }
+}
